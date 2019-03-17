15e6bed8a4ad73da41fef88f1eadba7c9b8a46f4
Manasapujha GR <manasapujha.gr@cerner.com>
2019-03-17 19:59:21 +0530

    Initialization and destroy of beans

--- HelloSpring/src/com/tutorialspoint/HelloWorld.java
+++ HelloSpring/src/com/tutorialspoint/HelloWorld.java
@@ -13,4 +13,14 @@ public class HelloWorld
     {
         System.out.println("Your Message : " + message);
     }
+
+    public void init()
+    {
+        System.out.println("Bean is going through init.");
+    }
+
+    public void destroy()
+    {
+        System.out.println("Bean will destroy now.");
+    }
 }
--- HelloSpring/src/com/tutorialspoint/MainApp.java
+++ HelloSpring/src/com/tutorialspoint/MainApp.java
@@ -1,14 +1,61 @@
 package com.tutorialspoint;
 
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.AbstractApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class MainApp
 {
     public static void main(String[] args)
     {
-        ApplicationContext context = new ClassPathXmlApplicationContext("Beans.xml");
-        HelloWorld obj = (HelloWorld) context.getBean("helloWorld");
-        obj.getMessage();
+        // Normal instantiation
+
+        ApplicationContext context1 = new ClassPathXmlApplicationContext("Beans_first.xml");
+        HelloWorld obj1 = (HelloWorld) context1.getBean("helloWorld");
+        obj1.getMessage();
+
+        ((AbstractApplicationContext) context1).registerShutdownHook();
+
+        /*
+         * ((AbstractApplicationContext) context1).registerShutdownHook();
+         * Bean is going through init.
+         * Your Message : Hello World!
+         * Your Message : I'm object A
+         * Your Message : I'm object A
+         * Your Message : I'm object A
+         * Your Message : null
+         * Bean will destroy now.
+         */
+
+        /*
+         * ((AbstractApplicationContext) context1).close();
+         * Bean is going through init.
+         * Your Message : Hello World!
+         * Bean will destroy now.
+         * Your Message : I'm object A
+         * Your Message : I'm object A
+         * Your Message : I'm object A
+         * Your Message : null
+         */
+
+        // Singleton
+        ApplicationContext context2 = new ClassPathXmlApplicationContext("Beans_Singleton.xml");
+        HelloWorld objA = (HelloWorld) context2.getBean("helloWorld");
+
+        objA.setMessage("I'm object A");
+        objA.getMessage();
+
+        HelloWorld objB = (HelloWorld) context2.getBean("helloWorld");
+        objB.getMessage();
+
+        // Prototype
+        ApplicationContext context3 = new ClassPathXmlApplicationContext("Beans_Prototype.xml");
+        HelloWorld objA3 = (HelloWorld) context3.getBean("helloWorld");
+
+        objA3.setMessage("I'm object A");
+        objA3.getMessage();
+
+        HelloWorld objB3 = (HelloWorld) context3.getBean("helloWorld");
+        objB3.getMessage();
     }
 }
